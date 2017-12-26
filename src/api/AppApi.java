167be09e3809b84235ae6b5e909e6570d4d8d047
김재섭 <leppk@taehwainno.com>
2017-12-27 00:26:00 +0900

    암호화jar 파일의 인터페이스 역활의 프로젝트 최초등록

---
+++ src/api/AppApi.java
@@ -0,0 +1,15 @@
+package api;
+
+/**
+ * 어플리케이션 API이다.
+ * 
+ * @author O117012
+ */
+public interface AppApi {
+	
+	/**
+	 * 자원정보를 반환한다.
+	 * @return 자원정보
+	 */
+	public abstract String getResourceInfo();
+}
